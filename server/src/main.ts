5a1099e3ce9555d304af9a9ba02278de47bf4c10
anookday <anookday@gmail.com>
2021-12-24 19:05:34 +0900

    enabled tracking client ip address

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,16 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
 import * as helmet from 'helmet'
 import * as cookieParser from 'cookie-parser'
 import * as userAgent from 'express-useragent'
+import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.use(helmet())
   app.use(cookieParser())
   app.use(userAgent.express())
+  app.set('trust proxy', true)
   await app.listen(5000)
 }
 bootstrap()
